964fc672cbef87422395678e6bb603a525d0d00a
Pandher <34991259+APandher@users.noreply.github.com>
2019-03-25 21:05:46 -0400

    Modularizing the clicks

--- Runescape Nightmare Zone Bot.py
+++ Runescape Nightmare Zone Bot.py
@@ -13,21 +13,21 @@ def hpReset():
     startTime1 = time.time()
     prayCord = pyautogui.locateOnScreen('Nightmare Zone Bot Pictures/Prayer Protect.png')
     a,b = pyautogui.center(prayCord)
-    pyautogui.click(pyautogui.moveTo(random.randint(a-5,a-2),random.randint(b-5,b-2)), duration = random.uniform(0.5,1))
+    click(a,b)
     time.sleep(random.uniform(1,2))
-    pyautogui.click(pyautogui.moveTo(random.randint(a-5,a-2),random.randint(b-5,b-2)), duration = random.uniform(0.5,1))
+    click(a,b)
 
 def overload():
     overloadCoordinates = overloadList[0]
     c,d = pyautogui.center(overloadCoordinates)
-    pyautogui.click(pyautogui.moveTo(random.randint(c-2,c),random.randint(d,d+2)),duration = random.uniform(1,1.5))
+    click(c,d)
     overloadList.append(overloadList[0])
     overloadList.remove(overloadList[0])
 
 def absorption():
     absorptionCoordinates = absorptionList[0]
     e,f = pyautogui.center(absorptionCoordinates)
-    pyautogui.click(pyautogui.moveTo(random.randint(e-2,e),random.randint(f,f+2)),duration = random.uniform(1,1.5))
+    click(e,f)
     absorptionList.append(absorptionList[0])
     absorptionList.remove(absorptionList[0])
 
@@ -44,11 +44,10 @@ def initialization():
 def logout():
     logout1Cord = pyautogui.locateOnScreen('Nightmare Zone Bot Pictures/logout1.png')
     g,h = pyautogui.center(logout1Cord)
-    pyautogui.click(pyautogui.moveTo(random.randint(g-2,g),random.randint(h,h+2)),duration = random.uniform(1,1.5))
+    click(g,h)
     logout2Cord = pyautogui.locateOnScreen('Nightmare Zone Bot Pictures/logout2.png')
     i,j = pyautogui.center(logout2Cord)
-    pyautogui.click(pyautogui.moveTo(random.randint(i-2,i),random.randint(j,j+2)),duration = random.uniform(1,1.5))
-
+    click(i,j)
 
 
 
@@ -65,6 +64,9 @@ def main():
 
     if pyautogui.locateOnScreen('Nightmare Zone Bot Pictures/logout.png') == True:
         logout()
+        
+ def click(e,f):
+    pyautogui.click(pyautogui.moveTo(random.randint(e-2,e),random.randint(f,f+2)),duration = random.uniform(1,1.5))
 
 
 
